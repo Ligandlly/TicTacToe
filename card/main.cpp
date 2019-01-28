c894d70a6d700760481a824e2a501dcbcb928fc6
冰凌 <bingling@binglingdemacbook-pro.local>
2019-01-28 16:58:36 +0800

    Initial Commit

---
+++ card/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  card
+//
+//  Created by 冰凌 on 2019/1/28.
+//  Copyright © 2019 lly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
